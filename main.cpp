7254e927b6fe58f4ad9670e6221d8ffd2a2f7a26
Erick Madrigal Ríos <erickmadrigalrios@gmail.com>
2015-04-13 00:11:41 -0600

    creo que ya

--- main.cpp
+++ main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char ** argv){
 }
 
 void* sumarFilas(void* indicadorP){
-    int indicador = (int)indicadorP;
+    int indicador = (long)indicadorP;
     int i,j;
     if(indicador%2){
         i = 0;
